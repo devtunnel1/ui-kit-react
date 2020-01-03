eb6f78164e43c5e17dceb96cff6d76d4000d572c
Akio Satoh <devtunnel@gmail.com>
2020-01-03 00:49:27 -0800

    Moved components into own files

---
+++ src/components/ColorHeadingTwo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class ColorHeadingTwo extends Component {
+  static propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
+  }
+
+  render() {
+    const {
+      text,
+      color
+    } = this.props
+
+    return (
+      <h2 style={{ color }}>
+        {text}
+      </h2>
+    )
+  }
+}
